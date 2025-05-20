8c16af89e0604fba17e9f7ae089c008c59fc9ecd
d.c! <daniel.c99999@gmail.com>
2025-05-20 17:27:14 +0300

    update class person

--- Person.cs
+++ Person.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Project__Analiza
 {
-    internal class Person
+    abstract class Person
     {
+        protected string first_name;
+        protected string last_name;
+        protected int age;
+
+        public  Person(string first_name  , string last_name, int age)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.age = age;
+        }
+
+
+
     }
 }
