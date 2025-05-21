361ae72f6439254c33af79210225656d67ea4cb6
d.c! <daniel.c99999@gmail.com>
2025-05-21 10:32:45 +0300

    update Attack_options class and more class for this

---
+++ M109_Artillery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project__Analiza
+{
+    internal class M109_Artillery
+    {
+    }
+}
